32e5089e030b7b9162db7c613b73a8801ff406df
CodeJack <pawan.mamoria92@gmail.com>
2023-01-17 14:25:42 +0530

    func done

--- assets/js/wd-ajax-load/js/load-posts.js
+++ assets/js/wd-ajax-load/js/load-posts.js
@@ -1,30 +1,36 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+  /**
+   * Load new post when the link is clicked.
+   */
+  $(".portfolio-item").click(function () {
+    var that = this;
 
-	/**
-	 * Load new post when the link is clicked.
-	 */
-	$('.portfolio-item').click(function() {
-	    var that = this;
-	    
-	    $('body').css('cursor', 'wait');
-	    $(this).append('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
-	    
-	    var $post_id = $(this).attr('id');
-		
-			// Show that we're working.
-			$('#project-info').html('<div class="text-center"><div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div></div>');
-      
-      $('#project-info').load("?p=" + $post_id + ' #content',
-        function() {
-          
-          $("html, body, .page-section-current").animate({ scrollTop: "0" },1000);
-          $('#project-info').removeClass('hide');
-          $('#project-info').css('display' , 'block');
-          $('body').css('cursor', 'auto');
-          
-          $(that).find('.ajax-progress-throbber').remove();
-        }
-      );	
-		return false;
-	});
+    $("body").css("cursor", "wait");
+    $(this).append(
+      '<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>'
+    );
+
+    var $post_id = $(this).attr("id");
+    console.log($post_id);
+    // Show that we're working.
+    $("#project-info").html(
+      '<div class="text-center"><div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div></div>'
+    );
+
+    $("#project-info").load(
+      "?p=" + $post_id + " #content-" + $post_id,
+      function () {
+        $("html, body, .page-section-current").animate(
+          { scrollTop: "0" },
+          1000
+        );
+        $("#project-info").removeClass("hide");
+        $("#project-info").css("display", "block");
+        $("body").css("cursor", "auto");
+
+        $(that).find(".ajax-progress-throbber").remove();
+      }
+    );
+    return false;
+  });
 });
